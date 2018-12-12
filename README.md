f7a53c6061e68ed7dd9157b12de6f87a4bd16a1f
oyama919 <oyama919@gmail.com>
2018-12-12 09:11:29 +0900

    create http and api

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # scala-http-test
+下記記事をみて動かしてみる
+https://qiita.com/yabaiwebyasan/items/a4a8be77f39d4a357093#%E3%83%97%E3%83%AD%E3%82%B8%E3%82%A7%E3%82%AF%E3%83%88%E3%81%AE%E4%BD%9C%E6%88%90
---
+++ src/main/scala/connection/MySQLDBImpl.scala
@@ -0,0 +1,12 @@
+package connection
+import slick.driver.MySQLDriver
+
+trait MySQLDBImpl {
+  val driver = MySQLDriver
+  import driver.api._
+  val db: Database = MySqlDB.connectionPool
+}
+private[connection] object MySqlDB {
+  import slick.driver.MySQLDriver.api._
+  val connectionPool = Database.forConfig("mysql")
+}
